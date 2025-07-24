919ddf81b14e890c6e077c9937965d3b581200e6
Ader-EX <musyaffa262@gmail.com>
2025-07-24 14:44:54 +0700

    feat : added login

--- main.py
+++ main.py
@@ -1,7 +1,33 @@
 from fastapi import FastAPI
+from fastapi.dependencies import models
+from fastapi.params import Depends
+from sqlalchemy.orm import Session
+from starlette import status
 
-app = FastAPI()
+from starlette.exceptions import HTTPException
+from schemas.UserSchemas import UserCreate, TokenSchema, RequestDetails
+from database import Base,engine,SessionLocal
+from models.User import User
+from utils import get_hashed_password, verify_password, create_access_token, create_refresh_token
 
+ACCESS_TOKEN_EXPIRE_MINUTES = 30  # 30 minutes
+REFRESH_TOKEN_EXPIRE_MINUTES = 60 * 24 * 7 # 7 days
+ALGORITHM = "HS256"
+JWT_SECRET_KEY = "narscbjim@$@&^@&%^&RFghgjvbdsha"   # should be kept secret
+JWT_REFRESH_SECRET_KEY = "13ugfdfgh@#$%^@&jkl45678902"
+
+from models.User import User
+Base.metadata.create_all(engine)
+
+
+
+def get_session():
+    session = SessionLocal()
+    try:
+        yield session
+    finally:
+        session.close()
+app=FastAPI()
 
 @app.get("/")
 async def root():
@@ -11,3 +37,41 @@ async def root():
 @app.get("/hello/{name}")
 async def say_hello(name: str):
     return {"message": f"Hello {name}"}
+
+
+@app.post("/register")
+def register_user(user: UserCreate, session: Session = Depends(get_session)):
+    existing_user = session.query(User).filter_by(username=user.username).first()
+    if existing_user:
+        raise HTTPException(status_code=400, detail="Email already registered")
+
+    encrypted_password =get_hashed_password(user.password)
+
+    new_user = User(username=user.username, password=encrypted_password )
+
+    session.add(new_user)
+    session.commit()
+    session.refresh(new_user)
+
+    return {"message":"user created successfully"}
+
+
+@app.post('/login' ,response_model=TokenSchema)
+def login(request: RequestDetails, db: Session = Depends(get_session)):
+    user = db.query(User).filter(User.username == request.username).first()
+    if user is None:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Incorrect email")
+    hashed_pass = user.password
+    if not verify_password(request.password, hashed_pass):
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Incorrect password"
+        )
+
+    access=create_access_token(user.id)
+    refresh = create_refresh_token(user.id)
+
+    return {
+        "access_token": access,
+        "refresh_token": refresh,
+    }
